8699a271276350399b03e022a699d397be8a3c2d
badluckbert <62387120+badluckbert@users.noreply.github.com>
2020-10-13 13:55:59 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -22,7 +22,8 @@ function add1 (elem)
     elem.children[2].innerHTML = currentScore + 1;
     elem.children[3].innerHTML = (currentScore + 1) - Number.parseInt(par);
   }
-    setTotals(elem);  
+totals.children[2].innerHTML = Number.parseInt(totals.children[2].innerHTML) + 1;
+totals.children[3].innerHTML = Number.parseInt(totals.children[3].innerHTML) + 1;
 }
 
 function subtract1 (elem) 
@@ -32,6 +33,12 @@ function subtract1 (elem)
   {
     elem.children[2].innerHTML = "-";
     elem.children[3].innerHTML = "-";
+    totals.children[2].innerHTML = Number.parseInt(totals.children[2].innerHTML) - 1;
+    totals.children[3].innerHTML = Number.parseInt(totals.children[3].innerHTML) - 1;
+  }
+  else if (elem.children[2].innerHTML == "-")
+  {
+    break;
   }
   else
   {
@@ -39,30 +46,15 @@ function subtract1 (elem)
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore - 1;
     elem.children[3].innerHTML = (currentScore - 1) - Number.parseInt(par);
-  }
-  setTotals(elem);
-}
-
-function clear (elem) {
-  if (totals.children[2].innerHTML == elem.children[2].innerHTML){
-    totals.children[2].innerHTML = "-";
-    totals.children[3].innerHTML = "-";
-  }
-  elem.children[2].innerHTML == "-";
-  elem.children[3].innerHTML == "-";
-  setTotals(elem);
-}
-
-function setTotals (elem)
-{
-  if (elem.children[2].innerHTML == "-")
-  {
     totals.children[2].innerHTML = Number.parseInt(totals.children[2].innerHTML) - 1;
     totals.children[3].innerHTML = Number.parseInt(totals.children[3].innerHTML) - 1;
   }
-  else
-  {
-    totals.children[2].innerHTML = Number.parseInt(totals.children[2].innerHTML) + Number.parseInt(elem.children[2].innerHTML);
-    totals.children[3].innerHTML = Number.parseInt(totals.children[3].innerHTML) + Number.parseInt(elem.children[3].innerHTML);
-  }
+}
+
+function clear (elem) 
+{
+  totals.children[2].innerHTML = Number.parseInt(totals.children[2].innerHTML) - Number.parseInt(elem.children[2].innerHTML);
+totals.children[3].innerHTML = Number.parseInt(totals.children[3].innerHTML) - Number.parseInt(elem.children[3].innerHTML);
+  elem.children[2].innerHTML == "-";
+  elem.children[3].innerHTML == "-";
 }
