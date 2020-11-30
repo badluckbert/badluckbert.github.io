84b69deace5ef98ac0e2e76c695daa46f44e887a
badluckbert <62387120+badluckbert@users.noreply.github.com>
2020-11-30 00:51:35 -0500

    Update assignment06.js

--- assignments/assignment06.js
+++ assignments/assignment06.js
@@ -12,38 +12,42 @@ var loans = [
 
 function loadDoc() {
   
-  // pre-fill defaults for first loan year
-  $(document).ready(function(){
-    var defaultYear = loans[0].loan_year;
-    $("#loan_year0" + 1).val = defaultYear++;
-    var defaultLoanAmount = loans[0].loan_amount;
+  $(document).ready(function(){ //Added the line to begin the jQuery statements
+    var defaultYear = parseInt(loans[0].loan_year); //These variable lines are staying the same
+    var checkYear = /^(196[3-9]|19[7-9]\d|200\d|20[1-4]\d)$/; //This is to validate the year entered. The range is 1963 to 2049. From SVSU's founding to a realistic future year for loan payoff.
+    var defaultLoanAmount = loans[0].loan_amount; //Continue this pattern. This doesn't change unless there is some special way to declare variables in jQuery that I don't know. 
     $("#loan_amt0" + 1).val = defaultLoanAmount.toFixed(2);
     var defaultInterestRate = loans[0].loan_int_rate;
     $("#loan_int0" + 1).val = defaultInterestRate;
-    var loanWithInterest = loans[0].loan_amount * (1 + loans[0].loan_int_rate);
-    $("#loan_bal0" + 1).html= toComma(loanWithInterest.toFixed(2));
-  
-  // pre-fill defaults for other loan years
+    var loanWithInterest = (loans[0].loan_amount * (1 + loans[0].loan_int_rate));
+    $("#loan_bal0" + 1).text= toComma(loanWithInterest.toFixed(2));
+    $("#loan_year0" + 1).val = defaultYear++; //$ to begin the jQuery, then changed .value to .val
+    
+    
     for (var i = 2; i < 6; i++) {
-      $("#loan_year0" + i).val(defaultYear++);
-      $("#loan_year0" + i).prop("disabled", true);
-      $("#loan_year0" + i).css('background-color', 'gray');
-      $("#loan_year0" + i).css('color', 'white');
+      $("#loan_year0" + i).val(defaultYear++); //In similar fashion to the previous block,
+      $("#loan_year0" + i).prop("disabled", true); //Add $ to start the jQuery statement,
+      $("#loan_year0" + i).css('background-color', 'gray'); //and convert each method to the jQuery version. 
+      $("#loan_year0" + i).css('color', 'white'); // For example, .color becomes .ccs.
       $("#loan_amt0" + i).val(defaultLoanAmount.toFixed(2));
       $("#loan_int0" + i).val(defaultInterestRate);
       $("#loan_int0" + i).prop("disabled", true);
       $("#loan_int0" + i).css('background-color', 'gray');
       $("#loan_int0" + i).css('color', 'white');
       loanWithInterest = ((loanWithInterest + defaultLoanAmount) * (1 + defaultInterestRate));
-      $("loan_bal0" + i).hmtl = toComma(loanWithInterest.toFixed(2));
+      $("loan_bal0" + i).text = toComma(loanWithInterest.toFixed(2));
     } // end: "for" loop
-
   
   // all input fields: select contents on fucus
   $("input[type=text]").focus(function() {
     $(this).select();
     $(this).css("background-color", "yellow");
-  }); 
+  });
+  if(defaultYear != checkYear){ //This uses the regex from earlier to check if the input year matches the specified set.
+      $('#result') // if it doesn't, an error message will appear. However, I can't get the message to not appear. 
+        .css('color','red')
+        .text("Please enter a year from SVSU's founding in 1963 or later.");
+  }
   $("input[type=text]").blur(function() {
     $(this).css("background-color", "white");
   });
@@ -63,6 +67,7 @@ function toComma(value) {
 
 function updateLoansArray() {
   loans[0].loan_year = parseInt($("#loan_year01").val());
+  
   for(var i=1; i<5; i++) {
     loans[i].loan_year = loans[0].loan_year + i;
     $("#loan_year0"+ (i+1) ).val(loans[i].loan_year);
